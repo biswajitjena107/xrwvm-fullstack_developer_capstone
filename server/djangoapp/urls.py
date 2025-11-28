999f9fb9c2f314a608550f2926559db568b48256
Biswajit Jena <biswajitjena10799910@gmail.com>
2025-11-28 09:17:07 -0500

    Completed lab-Create Django Proxy Services

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,9 +16,16 @@ urlpatterns = [
     path(route='register', view=views.registration, name='register'),
     # path for get car
         path(route='get_cars', view=views.get_cars, name ='getcars'),
+    # path for get_dealership view
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    # path for dealer_details view
+        path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
 
     # path for dealer reviews view
+        path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
 
     # path for add a review view
+    
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
