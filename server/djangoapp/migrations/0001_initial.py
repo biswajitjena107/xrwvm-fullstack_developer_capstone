db2f2c4e56d268b309a1063d0317e5304a6a99cb
Biswajit Jena <biswajitjena10799910@gmail.com>
2025-11-29 05:10:38 -0500

    Fixing linting error part-10

--- server/djangoapp/migrations/0001_initial.py
+++ server/djangoapp/migrations/0001_initial.py
@@ -36,7 +36,8 @@ class Migration(migrations.Migration):
                 ('year', models.IntegerField()),
                 ('car_make',
                   models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
-                 to='djangoapp.carmake')
+                                     to='djangoapp.carmake'
+                                 )
                 ),
             ],
         ),
